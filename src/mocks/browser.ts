89820e3f55745796476ed292fd6a741ae80d7a13
Kingsley Ifijeh <kinxly@gmail.com>
2025-07-29 22:40:40 +0100

    feat: completed checkout page

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,9 +1,10 @@
 import { setupWorker } from "msw/browser";
 
+import { cartHandlers } from "./handlers/cart/cart";
 import { dashboardHandler } from "./handlers/dashboard/analytics";
 import { orderHandlers } from "./handlers/order/orders";
 import { productHandlers } from "./handlers/products/products";
 
-const handlers = [...productHandlers, ...dashboardHandler, ...orderHandlers];
+const handlers = [...productHandlers, ...cartHandlers, ...dashboardHandler, ...orderHandlers];
 // This configures a Service Worker with the given request handlers.
 export const worker = setupWorker(...handlers);
