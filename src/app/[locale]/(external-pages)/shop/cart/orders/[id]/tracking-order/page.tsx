1780859de90de0feec1ef03517cad931e779089d
Kingsley Ifijeh <kinxly@gmail.com>
2025-09-26 22:43:53 +0100

    feat: fixed an dcleanup home page

--- src/app/[locale]/(external-pages)/shop/cart/orders/[id]/tracking-order/page.tsx
+++ src/app/[locale]/(external-pages)/shop/cart/orders/[id]/tracking-order/page.tsx
@@ -60,11 +60,10 @@ export default function TrackingPage({ params }: TrackingPageProperties) {
   };
 
   // eslint-disable-next-line unicorn/consistent-function-scoping
-  const handleRateRider = async (rating: number, review?: string) => {
-    // TODO: Implement rider rating API call
-    // eslint-disable-next-line no-console
-    
-  };
+  // const handleRateRider = async (rating: number, review?: string) => {
+  //   // TODO: Implement rider rating API call
+  //   // eslint-disable-next-line no-console
+  // };
 
   if (isLoading) {
     return (
@@ -182,7 +181,7 @@ export default function TrackingPage({ params }: TrackingPageProperties) {
           trackingData={trackingData}
           apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}
           onStatusUpdate={handleStatusUpdate}
-          onRateRider={handleRateRider}
+          // onRateRider={handleRateRider}
         />
       </Wrapper>
     </div>
